5da32c9815dc685e77b8135b398131b8605ed082
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-16 17:49:06 +0100

    Add updated dist

--- clients/webserver/public/js/dist/main.js
+++ clients/webserver/public/js/dist/main.js
@@ -18,38 +18,28 @@
       ;
     });
 })();
-;angular.module('GHome').controller('HouseMapCtrl', ['$scope', function($scope) {
-  $scope.rooms = [
-      {
-          "name": "Salle à manger",
-          "presence": "pres_mod_1",
-          "temperature": "temp_mod_1",
-          "brightness": "bright_mod_1",
-          "polygon": [
-              { "x": 10, "y": 15 },
-              { "x": 61, "y": 32 },
-              { "x": 42, "y": 12 },
-              { "x": 13, "y": 50 },
-              { "x": 50, "y": 54 }
-          ]
-      }, {
-          "name": "Cuisine",
-          "presence": "pres_mod_2",
-          "temperature": "temp_mod_2",
-          "brightness": "bright_mod_2",
-          "polygon": [
-              { "x": 79, "y": 23 },
-              { "x": 28, "y": 61 },
-              { "x": 70, "y": 75 },
-              { "x": 47, "y": 70 },
-              { "x": 72, "y": 28 }
-          ]
-      }
-  ];
+;function HouseMapCtrl($scope, HouseMapService) {
+  // Minimal bbox used for svg display
+  $scope.minX = 0;
+  $scope.minY = 0;
+  $scope.maxX = 0;
+  $scope.maxY = 0;
+
+  // Get the rooms (asynchronous)
+  HouseMapService.getRooms().then(function(rooms) {
+    $scope.rooms = rooms;
+  });
 
   $scope.polygonPoints = function(polygon) {
     var pointsRepr = '';
     angular.forEach(polygon, function(point, i) {
+      // Update min/max coordinates
+      if (point.x < $scope.minX) { $scope.minX = point.x; }
+      else if (point.x > $scope.maxX) { $scope.maxX = point.x; }
+      if (point.y < $scope.minY) { $scope.minY = point.y; }
+      else if (point.y > $scope.maxY) { $scope.maxY = point.y; }
+
+      // Update the points representation
       pointsRepr += point.x + ',' + point.y;
       if (i < polygon.length - 1) {
         pointsRepr += ' ';
@@ -57,4 +47,41 @@
     });
     return pointsRepr;
   };
-}]);
+}
+;angular.module('GHome').factory('HouseMapService', function($q, $timeout) {
+  var service = {};
+
+  // Replace with an AJAX call
+  service.getRooms = function() {
+    var deferred = $q.defer();
+    $timeout(function() {
+      deferred.resolve([
+        {
+            "name": "Salle à manger",
+            "presence": "pres_mod_1",
+            "temperature": "temp_mod_1",
+            "brightness": "bright_mod_1",
+            "polygon": [
+                { "x": 10, "y": 15 },
+                { "x": 60, "y": 15 },
+                { "x": 60, "y": 85 },
+                { "x": 10, "y": 85 },
+            ]
+        }, {
+            "name": "Cuisine",
+            "presence": "pres_mod_2",
+            "temperature": "temp_mod_2",
+            "brightness": "bright_mod_2",
+            "polygon": [
+                { "x": 10, "y": 115 },
+                { "x": 200, "y": 115 },
+                { "x": 200, "y": 160 },
+                { "x": 10, "y": 160 }
+            ]
+        }
+      ]);
+    }, 100);
+    return deferred.promise;
+  };
+  return service;
+});
