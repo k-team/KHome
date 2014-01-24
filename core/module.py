c904cf4991ed2906cd4f3a11a08121cb619a159e
TurpIF <pierre.turpin@insa-lyon.fr>
2014-01-24 09:50:20 +0100

    Ajout de la fonction use_module pour ajouter un module externe en réseau

--- core/module.py
+++ core/module.py
@@ -1,6 +1,13 @@
 import threading
 import fields, fields.io, fields.persistant, time
 
+# def prop_netfield():
+#     dict = creerjson(args)
+#     self.socket.write(json.dumps(dict))
+#     data_received = self.socket.recv()
+#     data_json = json.loads(data_received)
+#     comprendrejson
+
 def prop_field(field):
     def _prop_field(*args, **kwargs):
         if len(args) == 1 and not kwargs:
@@ -15,24 +22,31 @@ def prop_field(field):
         raise Exception
     return _prop_field
 
-def read_field(field):
-    def _read_field(**kwargs):
-        return field.read(**kwargs)
-    return _read_field
+def get_network_fields(module):
+    return ['F2']
 
-def write_field(field):
-    def _write_field(value):
-        return field.write(value)
-    return _write_field
+def prop_network_field(field):
+    def _prop_network_field(*args, **kwargs):
+        if len(args) == 1 and not kwargs:
+            return False # write
+        elif not args:
+            if not kwargs:
+                return None # read()
+            if len(kwargs) == 1 and 't' in kwargs:
+                return None # read(t=..)
+            if len(kwargs) == 2 and 'fr' in kwargs and 'to' in kwargs:
+                return None # read(fr=.., to=..)
+        raise Exception
+    return _prop_network_field
 
-class ModuleMeta(type):
+class BaseMeta(type):
     ls_name = set()
 
     def __new__(cls, name, parents, attrs):
-        return super(ModuleMeta, cls).__new__(cls, name, parents, attrs)
+        return super(BaseMeta, cls).__new__(cls, name, parents, attrs)
 
     def __call__(self, *args, **kwargs):
-        obj = super(ModuleMeta, self).__call__(*args, **kwargs)
+        obj = super(BaseMeta, self).__call__(*args, **kwargs)
         cls = type(obj)
 
 # Gestion du nom du module
@@ -59,7 +73,7 @@ class ModuleMeta(type):
         return obj
 
 class Base(threading.Thread):
-    __metaclass__ = ModuleMeta
+    __metaclass__ = BaseMeta
 
     # module_name = 'Module'
 
@@ -87,6 +101,40 @@ class Base(threading.Thread):
             f.join(1)
         self.running = False
 
+class NetworkMeta(type):
+    def __new__(cls, name, parents, attrs):
+        return super(NetworkMeta, cls).__new__(cls, name, parents, attrs)
+
+    def __call__(self, *args, **kwargs):
+        obj = super(NetworkMeta, self).__call__(*args, **kwargs)
+        cls = type(obj)
+
+# Gestion du nom du module
+        if not hasattr(obj, 'module_name'):
+            if not hasattr(cls, 'module_name'):
+                setattr(obj, 'module_name', cls.__name__)
+            else:
+                setattr(obj, 'module_name', cls.module_name)
+
+# Gestion des fields du module
+        ls_field = get_network_fields(obj.module_name)
+        for field in ls_field:
+            setattr(obj, field, prop_network_field(field))
+
+        return obj
+
+class Network(object):
+    __metaclass__ = NetworkMeta
+
+    def __init__(self, **kwargs):
+        super(Network, self).__init__()
+
+        if 'name' in kwargs:
+            self.module_name = kwargs['name']
+
+def use_module(module_name):
+    return Network(name=module_name)
+
 if __name__ == '__main__':
     class M1(Base):
         class Field(fields.io.Readable,
@@ -96,6 +144,7 @@ if __name__ == '__main__':
             field_name = 'mon_nom'
 
             def _acquire_value(self):
+                return M2.F2()
                 return (int(time.time()) % 10) ** 2
 
         class F1(fields.io.Readable,
@@ -113,7 +162,7 @@ if __name__ == '__main__':
     print b.mon_nom(fr=time.time() - 5, to=time.time())
     for i in xrange(10):
         b.mon_nom(i)
-        time.sleep(0.1)
+        # time.sleep(0.1)
 
     print b.mon_nom(fr=time.time() - 0.5, to=time.time())
     print b.mon_nom()
@@ -124,11 +173,19 @@ if __name__ == '__main__':
 
     print a.mon_nom(fr=0, to=time.time())
 
-    b.start()
-    try:
-        while True:
-            print b.mon_nom()
-            time.sleep(0.4)
-    except KeyboardInterrupt:
-        b.stop()
-        b.join(1)
+    # b.start()
+    # try:
+    #     while True:
+    #         print b.mon_nom()
+    #         time.sleep(0.4)
+    # except KeyboardInterrupt:
+    #     b.stop()
+    #     b.join(1)
+
+    class M2(Network):
+        pass
+
+    M2 = use_module('M2')
+    print M2.module_name
+    print M2.__dict__
+    print M2.F2()
