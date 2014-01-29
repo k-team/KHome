8da39aa8bda9e74f893a4045c80ea988ada7b3f3
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-29 23:44:44 +0100

    Module description added

--- clients/webserver/public/js/main.js
+++ clients/webserver/public/js/main.js
@@ -1,19 +1,17 @@
 "use strict";
 
-(function() {
-  angular.module('GHome', ['ngRoute', 'angularFileUpload'])
-    .config(function($routeProvider, $locationProvider) {
-      $routeProvider
-        .when('/home', { templateUrl: 'partials/home.html' })
-        .when('/weather', { templateUrl: 'partials/weather.html' })
-        .when('/surveillance', { templateUrl: 'partials/surveillance.html' })
-        .when('/temperature', { templateUrl: 'partials/temperature.html' })
-        .when('/brightness', { templateUrl: 'partials/brightness.html' })
-        .when('/power', { templateUrl: 'partials/power.html' })
-        .when('/store', { templateUrl: 'partials/store.html' })
-        .when('/settings', { templateUrl: 'partials/settings.html' })
-        .when('/ai-config', { templateUrl: 'partials/ai-config.html' })
-        .otherwise({ redirectTo: '/home' })
-      ;
-    });
-})();
+angular.module('GHome', ['ngRoute', 'angularFileUpload'])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/home', { templateUrl: 'partials/home.html' })
+      .when('/weather', { templateUrl: 'partials/weather.html' })
+      .when('/surveillance', { templateUrl: 'partials/surveillance.html' })
+      .when('/temperature', { templateUrl: 'partials/temperature.html' })
+      .when('/brightness', { templateUrl: 'partials/brightness.html' })
+      .when('/power', { templateUrl: 'partials/power.html' })
+      .when('/store', { templateUrl: 'partials/store.html' })
+      .when('/settings', { templateUrl: 'partials/settings.html' })
+      .when('/ai-config', { templateUrl: 'partials/ai-config.html' })
+      .otherwise({ redirectTo: '/home' })
+    ;
+  });
