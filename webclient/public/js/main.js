35cb4fedf3f80212576a7b0df37be3e149ccb5c9
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-01-30 13:25:04 +0100

    Dynamic loading of modules (missing specific partials)

--- webclient/public/js/main.js
+++ webclient/public/js/main.js
@@ -2,16 +2,18 @@
 
 angular.module('GHome', ['ngRoute', 'angularFileUpload'])
   .config(function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/home', { templateUrl: 'partials/home.html' })
-      .when('/weather', { templateUrl: 'partials/weather.html' })
-      .when('/surveillance', { templateUrl: 'partials/surveillance.html' })
-      .when('/temperature', { templateUrl: 'partials/temperature.html' })
-      .when('/brightness', { templateUrl: 'partials/brightness.html' })
-      .when('/power', { templateUrl: 'partials/power.html' })
-      .when('/store', { templateUrl: 'partials/store.html' })
-      .when('/settings', { templateUrl: 'partials/settings.html' })
-      .when('/ai-config', { templateUrl: 'partials/ai-config.html' })
-      .otherwise({ redirectTo: '/home' })
-    ;
+    $routeProvider.when('/home', {
+      templateUrl: '/partials/home.html'
+    }).when('/store', {
+      templateUrl: '/partials/store.html'
+    }).when('/settings', {
+      templateUrl: '/partials/settings.html'
+    }).when('/modules', {
+      templateUrl: '/partials/modules.html'
+    }).when('/modules/:module_name', {
+      controller: 'ModuleInjectorCtrl',
+      templateUrl: '/partials/blank_module.html'
+    }).otherwise({
+      redirectTo: '/home'
+    });
   });
