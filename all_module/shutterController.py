42ac142567845242ad428c63e55816d0c5a81c56
samuel.carensac <samuel.carensac@gmail.com>
2014-01-31 11:11:42 +0100

    modules shutter

---
+++ all_module/shutterController.py
@@ -0,0 +1,20 @@
+from twisted.internet import reactor
+import core.module
+import core.fields
+import core.fields.io
+import core.fields.persistant
+import time
+import all_modules.shutter
+
+if __name__ == '__main__':
+
+        
+    class ShutterController(core.module.Base)
+        shutter= use_module('Shutter')
+        
+        def always(self):
+            #here do action
+            
+            
+        
+    #code du main a remettre lais il était chelou donc pr le moment je l'ai vire...
