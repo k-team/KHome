68cddc90108984ca27f00478aad365c480b6ab05
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-01-31 09:11:17 +0100

    ajout module porte

--- test_module.py
+++ test_module.py
@@ -44,6 +44,34 @@ if __name__ == '__main__':
                 core.fields.Base):
             pass
 
+    class PorteCapteur(core.module.Base):
+        update_rate = 10
+        class porte(
+            core.fields.sensor.Porte
+            core.fields.io.Readable,
+            core.fields.Base):
+        pass
+
+    class PorteActionneur(core.module.Base):
+        class porte(
+            core.fields.actuator.Porte
+            core.fields.io.Writable,
+            core.fields.Base):
+        pass
+
+    class PorteAcces(core.module.Base):
+        update_rate = 10
+        porteCapteur = use_module('PorteCapteur')
+        porteActionneur = use_module('PorteActionneur')
+
+        porte = fields.proxy.mix('PorteCapteur', 'porte', 'PorteActionneur', 'porte')
+
+        class Field(core.fields.proxy, core.fields.Base):
+            proxy_module_name = "PorteCapteur"
+            proxy_field_name = "porte"
+
+        pass
+
     b = M1()
 
     b.start()
