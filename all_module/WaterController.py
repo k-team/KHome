3591c31a05214096ba081fe6a976f212d22dbb8b
gcattan <gregoire.cattan@insa-lyon.fr>
2014-02-07 11:29:08 +0100

    bidule truc

--- all_module/WaterController.py
+++ all_module/WaterController.py
@@ -1,19 +1,22 @@
-from twisted.internet import reactor
-import core.module
-import core.fields
-import core.fields.io
-import core.fields.persistant
-import time
+import module
+import fields
+import fields.io
+import fields.persistant
+import fields.proxy
+import fields.sensor
+import fields.actuator
+from module import use_module
 
 class WaterController(core.module.Base):
     update_rate = 10
     water_valve_sensor = use_module('WaterValve')
     water_valve_actuator = use_module('WaterValveActuator')
     human_presence_sensor = use_module('HumanPresenceSensor')
-    class Controller(core.fields.Base):
-        if human_presence_sensor.Presence()
-            and water_valve_sensor.WaterValve() == 'FERME':
-                water_valve_actuator.WaterValve('OPEN')
-        if not human_presence_sensor.Presence()
-            and water_valve_sensor.WaterValve() == 'OPEN':
-                water_valve_actuator.WaterValve('CLOSE')
+    class controller(core.fields.Base):
+        def always(self):
+            if human_presence_sensor.Presence():
+                and water_valve_sensor.WaterValve() == 'FERME':
+                    water_valve_actuator.WaterValve('OPEN')
+            elif not human_presence_sensor.Presence()
+                and water_valve_sensor.WaterValve() == 'OPEN':
+                    water_valve_actuator.WaterValve('CLOSE')
