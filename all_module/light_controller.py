6a4a0127e23faf34663da5f8fc0c05c651aa942f
samuel.carensac <samuel.carensac@gmail.com>
2014-02-07 03:49:05 +0100

    just to pull

--- all_module/light_controller.py
+++ all_module/light_controller.py
@@ -4,17 +4,19 @@ import core.fields
 import core.fields.io
 import core.fields.persistant
 import time
-import all_modules.lightButton
+import all_modules.light_button
 
 if __name__ == '__main__':
 
         
     class LightController(core.module.Base)
-        light= use_module('Shutter')
+        light= use_module('LightButton')
+        presence= use_module('Presence')
         
         def always(self):
             #here do action
             
             
+            
         
-    #code du main a remettre lais il était chelou donc pr le moment je l'ai vire...
+    #code du main a remettre mais il etait chelou donc pr le moment je l'ai vire...
