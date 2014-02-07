fa0ee07bfb7f364e4e03d3886e96f66dc13af4ee
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-07 10:05:13 +0100

    Fix module name BAbyMonitoring

---
+++ all_module/MoistureController.py
@@ -0,0 +1,20 @@
+from twisted.internet import reactor
+import core.module
+import core.fields
+import core.fields.io
+import core.fields.persistant
+import time
+import all_modules.MoistureSensor
+import all_modules.FanActuator
+
+class MoistureController(core.module.Base):
+    update_rate = 10
+    MOISTURE_VALUE = 100
+    moisture_sensor = use_module('MoistureSensor')
+    fan_actuator = use_module('FanActuator')
+    class Controller(core.fields.Base):
+        def always(self):
+            if moisture_sensor.Moisture > MOISTURE_VALUE
+                fan_actuator.Fan(true)
+            else
+                fan_actuator.Fan(false)
