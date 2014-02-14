99d6e8642d44c515b541a97a8db8529a7d3d04b4
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-14 09:29:20 +0100

    ajout de la persitance pour les sensor

--- modules/human_presence_sensor/local_module.py
+++ modules/human_presence_sensor/local_module.py
@@ -2,9 +2,10 @@ import module
 import fields
 import fields.io
 import fields.sensor
+import fields.persistant
 
 class HumanPresenceSensor(module.Base):
     update_rate = 10
 
-    class presence(fields.sensor.Presence, fields.io.Readable, fields.Base):
+    class presence(fields.sensor.Presence, fields.io.Readable, fields.persistant.Volatile, fields.Base):
         pass
--- modules/methane_sensor/local_module.py
+++ modules/methane_sensor/local_module.py
@@ -2,9 +2,11 @@ import module
 import fields
 import fields.io
 import fields.sensor
+import fields.persistant
+
 
 class MethaneSensor(module.Base):
     update_rate = 10
 
-    class methane_presence(fields.sensor.MethanePresence, fields.io.Readable, fields.Base):
+    class methane_presence(fields.sensor.Methane, fields.io.Readable,  fields.persistant.Volatile, fields.Base):
         pass
