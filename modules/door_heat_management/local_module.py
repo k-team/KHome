73ee038106c8ef2664473c5ac49f0116da530601
gros zizi <gros@gros-VirtualBox.(none)>
2014-02-16 23:06:18 +0100

    correction du heat_manager_controller

--- modules/door_heat_management/local_module.py
+++ modules/door_heat_management/local_module.py
@@ -3,6 +3,8 @@ import fields.proxy
 
 class DoorHeatManagement(module.Base):
     update_rate = 10
+    temperature = fields.proxy.readable('temperature', 'TemperatureForecast', 'temperature')
+    door_actuator = fields.proxy.writable('door_actuator', 'DoorAccess', 'door')
 
-    management = fields.proxy.mix('management', 'TemperatureForecast',
-            'temperature', 'DoorAccess', 'door')
+#    management = fields.proxy.mix('management', 'TemperatureForecast',
+#            'temperature', 'DoorAccess', 'door')
