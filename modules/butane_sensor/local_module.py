cea7ce1dc75c677074fafa533308613b6ab6c815
gros zizi <gros@gros-VirtualBox.(none)>
2014-02-16 22:49:45 +0100

    résolution de problème gaz_controller

--- modules/butane_sensor/local_module.py
+++ modules/butane_sensor/local_module.py
@@ -3,12 +3,13 @@ import fields
 import fields.io
 import fields.sensor
 import fields.persistant
+import fields.syntax
 
 class ButaneSensor (module.Base):
     update_rate = 10
 
     class butane_presence(fields.sensor.Butane,
-            fields.syntax.Boolean,
+            fields.syntax.Numeric,
 	    fields.io.Readable,
             fields.persistant.Volatile,
             fields.Base):
