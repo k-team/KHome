ff2f3aeba991413463cca6f1a1c518648be84767
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-16 22:06:46 +0100

    fix module window_security

--- modules/window_security/local_module.py
+++ modules/window_security/local_module.py
@@ -1,16 +1,32 @@
 import module
 from module import use_module
 import fields
-import fields.io
-import fields.proxy
+import logging
 
 class WindowSecurity(module.Base):
     update_rate = 10
-
     windowAccess = use_module('WindowAccess')
     recognition = use_module('Recognition')
     alarmActuator = use_module('AlarmActuator')
 
-    # TODO What the fuck is that ?
-    security = fields.proxy.mix('security', 'WindowAccess', 'Window',
-            'Recognition', 'Recognised', 'AlarmActuator', 'Alarm')
+    class controller(fields.Base):
+        def __init__(self):
+            super(WindowSecurity.controller, self).__init__()
+
+        def always(self):
+            print 'testons'
+            try:
+                window_status = self.module.window_access.window()
+                recognition_status = self.module.recognition.recognised()
+                print 'window_status = %s, recognition_status = %s' % (window_status, recognition_status)
+            except TypeError as e:
+                logger = logging.getLogger()
+                logger.exception(e)
+            else:
+                if window_status == 'OPEN' and recognition_status == 'UNKNOWN':
+                    self.module.alarm_actuator.alarm(True)
+                    print 'Alert Unknown person in the house'
+                else:
+                    self.module.alarm_actuator.alarm(False)
+                    print 'Nothing to worry the house is safe'
+
