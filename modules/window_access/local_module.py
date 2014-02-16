f6cb94b0ee57d700be0232a1ffc27442f41746c7
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-16 22:10:32 +0100

    fix module window_access

--- modules/window_access/local_module.py
+++ modules/window_access/local_module.py
@@ -1,14 +1,8 @@
 import module
-from module import use_module
-import fields
-import fields.io
 import fields.proxy
 
 class WindowAccess(module.Base):
     update_rate = 10
-
-    windowSensor = use_module('WindowSensor')
-    windowActuator = use_module('WindowActuator')
 
     window = fields.proxy.mix('window', 'WindowSensor', 'window',
             'WindowActuator', 'window')
