cf991f18bf947d1225466ba0b270cbf0f1cd096c
gros zizi <gros@gros-VirtualBox.(none)>
2014-02-20 15:17:17 +0100

    capteur de son et butane

--- modules/sound_sensor/local_module.py
+++ modules/sound_sensor/local_module.py
@@ -7,6 +7,7 @@ import fields.syntax
 
 class SoundSensor(module.Base):
     update_rate = 10
+    public_name = 'Capteur de son'
 
     class sound(fields.syntax.Numeric, fields.sensor.Sound, fields.io.Readable, fields.persistant.Volatile, fields.Base):
         pass
