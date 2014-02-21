738f90f0bea2e7d29c90f8e0549333fd7e5d14ab
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-02-21 09:32:07 +0100

    Power plug actuator done

--- modules/electric_current_switch/local_module.py
+++
@@ -1,9 +0,0 @@
-import module
-from module import use_module
-import fields.proxy
-
-class ElectricCurrentSwitch(module.Base):
-    update_rate = 10
-    electric_current = fields.proxy.mix('electric_current',
-                                       'ElectricCurrentSensor', 'electric_current',
-                                   	   'ElectricCurrentActuator', 'electric_current')
