28738a759c53b9af12fbcc94073e498f9a420cc0
TurpIF <pierre.turpin@insa-lyon.fr>
2014-02-21 16:47:27 +0100

    Ajout d'icones + suppr de water_flush (va tirer toi meme la chasse)

--- modules/water_controller/local_module.py
+++ modules/water_controller/local_module.py
@@ -13,12 +13,13 @@ class WaterController(module.Base):
 
     class keep_water_on_duration(fields.syntax.BoundNumeric, fields.io.Writable,
             fields.io.Readable, fields.persistant.Volatile, fields.Base):
+        public_name = "Attente (s)"
         lower_bound = 0
         upper_bound = 10
 
         def on_start(self):
             super(WaterController.keep_water_on_duration, self).on_start()
-            self.emit_value((type(self).upper_bound-type(self).lower_bound)/2)
+            self.emit_value((type(self).upper_bound-type(self).lower_bound)/2.0)
 
     class controller(fields.io.Hidden, fields.Base):
         def always(self):
@@ -27,10 +28,10 @@ class WaterController(module.Base):
                 return
             limit = limit[1]
             presence = self.module.human_presence_sensor.presence(fr=limit, to=0)
-            if any(presence) and self.module.water_valve_sensor():
+            if any(map(lambda x: x[1], presence)) and self.module.water_valve_sensor()[1]:
                 self.module.water_valve_sensor(False)
 
     class water_valve_sensor(fields.sensor.WaterValve,
             fields.actuator.WaterValve, fields.persistant.Database,
             fields.Base):
-        public_name = 'Capteur de valve d\'eau'
+        public_name = 'Etat du robinet'
--- modules/water_flush/local_module.py
+++
@@ -1,6 +0,0 @@
-import module
-import fields.proxy
-
-class WaterFlush(module.Base):
-    update_rate = 10
-    flush = fields.proxy.mix('flush', 'HumanPresenceSensor', 'presence','PistonActuator', 'piston')
