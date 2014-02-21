9203bd96004def6faf8f9b4a50d8c30a5f39f714
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-21 08:59:14 +0100

    modif images + nom publique e human_presence_sensor

--- modules/human_presence_sensor/local_module.py
+++ modules/human_presence_sensor/local_module.py
@@ -7,6 +7,7 @@ import fields.syntax
 
 class HumanPresenceSensor(module.Base):
     update_rate = 1
+    public_name = 'Capteur de presence'
 
     class presence(fields.syntax.Boolean, fields.sensor.Presence, fields.io.Readable, fields.persistant.Volatile, fields.Base):
         pass
