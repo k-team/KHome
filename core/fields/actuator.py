de3c0b9fa93936c830d9811e64f752cf2fe2341f
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-02-21 11:14:39 +0100

    Franck je t'ai dit de la merde

--- core/fields/actuator.py
+++ core/fields/actuator.py
@@ -1,17 +1,24 @@
+import sys
+import logging
+import socket
+
 import io
 import syntax
 import persistant
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+_formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+_handler = logging.StreamHandler()
+_handler.setFormatter(_formatter)
+_handler.setLevel(logging.DEBUG)
+logger.addHandler(_handler)
+
 def Dummy(data_type):
-    class _Dummy(data_type,
-            # persistant.Volatile,
-            # io.Readable,
-            io.Writable):
+    class _Dummy(data_type, persistant.Volatile, io.Readable):
         def set_value(self, t, value):
             print t, value
-            return True
     return _Dummy
-
 
 Alarm = Dummy(syntax.Boolean)
 Door = Dummy(syntax.Boolean)
@@ -20,113 +27,49 @@ Fan = Dummy(syntax.Boolean)
 Gaz = Dummy(syntax.Boolean) #true = ouvert, flase = ferme
 LightButton = Dummy(syntax.Boolean)
 Piston = Dummy(syntax.Boolean)
-Shutter = Dummy(syntax.Numeric)
+Shutter = Dummy(syntax.Percentage)
 Temperature = Dummy(syntax.Numeric)
 WaterValve = Dummy(syntax.Boolean)
 Window = Dummy(syntax.Boolean)
 
-
-import sys
-import string
-from twisted.internet import protocol
-from twisted.python.log import startLogging
-from twisted.internet import reactor
-from twisted.internet.protocol import ClientCreator
-from twisted.internet.protocol import ClientFactory
-from twisted.internet.protocol import Protocol
-from twisted.internet.endpoints import TCP4ClientEndpoint
-
 class Actuator(object):
-    from twisted.internet import reactor
-    #host = '134.214.106.23'
-    #port = 5000
     host = '134.214.106.23'
     port = 5000
-    actuator_id = ""
-    org =""
-    sentData=""
-    connector=""
 
     def __init__(self):
         super(Actuator, self).__init__()
-        # reactor.connectTCP(type(self).host,
-        #         type(self).port,
-        #         ActuatorConnectionFactory(self,
-        #             type(self).actuator_id))
-        point =    TCP4ClientEndpoint(reactor,self.host,self.port)
-        self.deferred = point.connect(ActuatorConnectionFactory(self,self.actuator_id))
+        self.actuator_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.actuator_socket.connect((type(self).host, type(self).port))
+        self.actuator_socket_file = self.actuator_socket.makefile()
 
-
-    def sendData(self,data):
-        pass
-
-    def start(self):
-        self.connector = reactor.run()
-        #super(Actuator, self).start()
+    def set_value(self, t, value):
+        """
+        Start the actuator (eg. start sending via the reactor).
+        """
+        logger.info('setting the value to %s', value)
+        data = self.prepare_data(value)
+        self.actuator_socket_file.write(data)
+        self.actuator_socket_file.flush()
+        return super(Actuator, self).set_value(t, value)
 
     def close(self):
-        self.connector.disconnect()
-        #protocol.transport.loseConnection()
-        #reactor.stop()
-        super(Actuator, self).close()
-        
-        
-        
-        
-class ActuatorConnection(Protocol):
-    def __init__(self, actuator):
-        self.actuator = actuator
-        
-    def connectionMade(self):
-        print "Connexion etablished"
-        self.transport.write(self.actuator.sentData)
-        #self.transport.loseConnection()
-        #reactor.callLater(1, self.actuator.deferred.cancel)
-        self.actuator.deferred.cancel
-        # reactor.stop()
-        #self.transport.write(self.actuator.sentData)
+        """
+        Close/stop the actuator.
+        """
+        logger.info('closing the connection')
+        self.actuator_socket_file.close()
 
-    def dataReceived(self, data):
-        pass
-    def sendData(self):
-        self.transport.write(self.actuator.sentData)
+    def prepare_data(self, data):
+        """
+        Format data to send to the actuator, depending on its needed format.
+        """
+        raise NotImplementedError
 
-    def connectionLost(self, reason):
-        print "connection lost"
-        
-        
-class ActuatorConnectionFactory(ClientFactory):
-    def __init__(self, actuator,filter_id):
-        self.actuator = actuator
-        self.filter_id = filter_id
+class PowerPlug(Actuator, io.Writable):
+    actuator_id = 'FF9F1E07'
+    org = '05'
 
-    def clientConnectionFailed(self, connector, reason):
-        print "Connection failed - goodbye!"
-
-    def clientConnectionLost(self, connector, reason):
-        print "Connection lost - goodbye!"
-
-    def buildProtocol(self, addr):
-        return ActuatorConnection(self.actuator)
-        
-        
-        
-class PriseElectrique(Actuator):
-    actuator_id = "FF9F1E02"
-    #actuator_id = "FF9F1E03"
-    org = "05"
-    
-    
-    def sendData(self,data):
-        stat_check = "308E"
-        if data=="1": #activer
-            data = "50000000"
-        else:
-            data = "70000000"
-        self.sentData = "A55A6B"+self.org+data+self.actuator_id+stat_check  
-        self.start()
-        #self.stop()
-
-if __name__ == '__main__':
-   a = PriseElectrique()
-   a.sendData("1")
+    def prepare_data(self, data):
+        data = '50000000' if data else  '70000000'
+        return ''.join(('A55A6B', type(self).org, data,
+            type(self).actuator_id, '308E'))
