90ba4d3e3dbe3add8f7c82167f2b0b18fa29e3f8
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-02-21 10:23:13 +0100

    Small fixes on door_access

--- modules/door_access/local_module.py
+++ modules/door_access/local_module.py
@@ -1,19 +1,12 @@
 import module
-import fields.io
+import fields
 import fields.actuator
 import fields.sensor
 import fields.persistant
-import fields.syntax
-
 
 class DoorAccess(module.Base):
     update_rate = 10
-    
-    class door(#fields.syntax.Boolean,
-            fields.sensor.Contact,
-            fields.actuator.Door,
-            fields.persistant.Volatile,
-            fields.Base):
-        pass        
-    
-    
+
+    class door(fields.sensor.Contact, fields.actuator.Door,
+            fields.persistant.Volatile, fields.Base):
+        pass
