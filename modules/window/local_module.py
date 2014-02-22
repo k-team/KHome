6ee70f3d864778c6807c3300e592f2d6de767878
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-22 20:36:41 +0100

    module window fait

---
+++ modules/window/local_module.py
@@ -0,0 +1,13 @@
+import module
+import fields
+import fields.actuator
+import fields.sensor
+import fields.persistant
+
+class Door(module.Base):
+    public_name = 'Fenetre auto'
+    update_rate = 10
+
+    class state(fields.sensor.Contact, fields.actuator.Window,
+            fields.persistant.Volatile, fields.Base):
+        public_name = 'Etat de la fenetre'
