a70d04bc4a561c4b90fda2232d26a347645b0127
iviraragav <iler.viraragavane@insa-lyon.fr>
2014-02-22 20:31:56 +0100

    module window fait

--- modules/window_access/local_module.py
+++
@@ -1,8 +0,0 @@
-import module
-import fields.proxy
-
-class WindowAccess(module.Base):
-    update_rate = 10
-
-    window = fields.proxy.mix('window', 'WindowSensor', 'window',
-            'WindowActuator', 'window')
