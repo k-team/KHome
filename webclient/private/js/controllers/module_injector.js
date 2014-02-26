1ef2c1582cf0777b784451089ed38f2a0b47b507
TurpIF <pierre.turpin@insa-lyon.fr>
2014-02-26 11:34:12 +0100

    Test d'un truc

--- webclient/private/js/controllers/module_injector.js
+++ webclient/private/js/controllers/module_injector.js
@@ -6,12 +6,14 @@ function ModuleInjectorCtrl($scope, ModuleService, $routeParams, $compile, $http
   var loadModule = function() {
     $scope.loading = true;
     return ModuleService.moduleStatus($scope.moduleName).then(function(module) {
+      // injectView();
       $scope.loading = false;
       $scope.unreachable = false;
       $scope.module = module;
       $scope.$broadcast('module.statusUpdate', module);
       return module;
     }, function() {
+      // injectView();
       $scope.loading = false;
       $scope.unreachable = true;
     });
@@ -20,8 +22,11 @@ function ModuleInjectorCtrl($scope, ModuleService, $routeParams, $compile, $http
   loadModule();
 
   // Load the angular-like html to be injected
-  $http.get('/api/modules/' + $scope.moduleName + '/public/partial.html')
-    .then(function(result) { $('#inject').html($compile(result.data)($scope)); });
-  $http.get('/api/modules/' + $scope.moduleName + '/public/independant.html')
-    .then(function(result) { $('#inject-independant').html($compile(result.data)($scope)); });
+  var injectView = function() {
+    $http.get('/api/modules/' + $scope.moduleName + '/public/partial.html')
+      .then(function(result) { $('#inject').html($compile(result.data)($scope)); });
+    $http.get('/api/modules/' + $scope.moduleName + '/public/independant.html')
+      .then(function(result) { $('#inject-independant').html($compile(result.data)($scope)); });
+  };
+  injectView();
 }
