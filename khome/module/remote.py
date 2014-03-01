be305dd8df2c90bda927879982958a9a0144acc8
TurpIF <pierre.turpin@insa-lyon.fr>
2014-03-01 10:43:25 +0100

    Ajout d'un commentaire pour is_ready

--- khome/module/remote.py
+++ khome/module/remote.py
@@ -2,4 +2,8 @@ import os
 import path
 
 def is_ready(module_name):
+    """
+    Return if the module *module_name* is ready.
+    This is detected by watching the ready file of the module.
+    """
     return os.path.exists(path.ready_file(module_name))
